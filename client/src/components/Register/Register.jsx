2be18a07232938391a52871792b56425a4eba7b2
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-05-17 11:30:52 -0400

    styled registration page

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -27,67 +27,69 @@ export default class Register extends Component {
           this.props.history.push("/");
         }}
       >
-        <h3>Register</h3>
-        <label htmlFor="username">username:</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          value={username}
-          onChange={this.handleChange}
-        />
-        <br />
+        <div className="login-wrapper">
+          <h3>Register</h3>
+          <label htmlFor="username">username:</label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            value={username}
+            onChange={this.handleChange}
+          />
+          <br />
 
-        <label htmlFor="brand">brand:</label>
-        <input
-          id="brand"
-          type="text"
-          name="brand"
-          value={brand}
-          onChange={this.handleChange}
-        />
-        <br />
+          <label htmlFor="brand">brand:</label>
+          <input
+            id="brand"
+            type="text"
+            name="brand"
+            value={brand}
+            onChange={this.handleChange}
+          />
+          <br />
 
-        <label htmlFor="name">name:</label>
-        <input
-          id="name"
-          type="text"
-          name="name"
-          value={name}
-          onChange={this.handleChange}
-        />
-        <br />
+          <label htmlFor="name">name:</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <br />
 
-        <label htmlFor="email">email:</label>
-        <input
-          id="email"
-          type="text"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-        />
-        <br />
+          <label htmlFor="email">email:</label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <br />
 
-        <label htmlFor="phone">phone:</label>
-        <input
-          id="phone"
-          type="text"
-          name="phone"
-          value={phone}
-          onChange={this.handleChange}
-        />
-        <br />
+          <label htmlFor="phone">phone:</label>
+          <input
+            id="phone"
+            type="text"
+            name="phone"
+            value={phone}
+            onChange={this.handleChange}
+          />
+          <br />
 
-        <label htmlFor="password">password:</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-        />
-        <br />
-        <button>Submit</button>
+          <label htmlFor="password">password:</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button className="black-button">Submit</button>
+        </div>
       </form>
     );
   }
