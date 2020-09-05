7222b2a4b219adef75852f124174a4cb1c8e4cd2
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-09-04 21:09:51 -0400

    updated products display

--- client/src/components/ShowItems/ShowItems.jsx
+++ client/src/components/ShowItems/ShowItems.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./ShowItems.css";
 import "../BrandItem/BrandItem.css";
 
@@ -7,17 +6,19 @@ export default function ShowItems(props) {
   return (
     <>
       <h3>All Clothing</h3>
-      {props.items.map((item) => (
-        <div className="showitems-wrapper">
-          <div>
-            <img src={item.pic1} />
-          </div>
+      <div className="products-display">
+        {props.items.map((item) => (
+          <div className="showitems-wrapper">
+            <div>
+              <img src={item.pic1} />
+            </div>
 
-          <div classname="show-item-text">
-            {item.product_name} ${item.price}
+            <div classname="show-item-text">
+              {item.product_name} ${item.price}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 }
